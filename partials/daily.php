b782c3a5128b63e60ea99cfbde88991dc9dfa588
mikey <michaelrayvon@gmail.com>
2015-05-14 09:36:57 -0500

    add url case for subdomain

--- partials/daily.php
+++ partials/daily.php
@@ -3,5 +3,9 @@ $daily_sql = "SELECT username FROM daily";
 $daily_query = mysqli_query( $conn, $daily_sql );
 $daily_row = mysqli_fetch_array($daily_query, MYSQL_ASSOC);
 $daily_username = $daily_row['username'];
-$daily_url = 'http://localhost:8888/odie/?u=' . $daily_username;
+if ($home == 'odie.us') {
+	$daily_url = 'http://' . $daily_username . '.' . $home;
+} else {
+	$daily_url = $home . '?u=' . $daily_username;
+}
 $daily_output = '&#10024; <a class="daily" href="' . $daily_url . '">' . $daily_username . '</a> &#10024;';
--- partials/rand.php
+++ partials/rand.php
@@ -7,5 +7,9 @@ $random_sql = "SELECT * FROM $table ORDER BY RAND() LIMIT 0,1";
 $random_query = mysqli_query( $conn, $random_sql );
 $random_row = mysqli_fetch_array($random_query, MYSQL_ASSOC);
 $random_username = $random_row['username'];
-$random_url = 'http://localhost:8888/odie/?u=' . $random_username;
+if ($home == 'odie.us') {
+	$random_url = 'http://' . $random_username . '.' . $home;
+} else {
+	$random_url = $home . '?u=' . $random_username;
+}
 $random_output = '<a href="' . $random_url . '" id="rand">.</a>';
