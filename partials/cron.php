4d6bd3b82b812cf04f71cbee4c793c70889fc368
mikey <michaelrayvon@gmail.com>
2015-05-14 09:36:13 -0500

    include vars.  add comments.  add cron argv condition.  add else.

--- partials/cron.php
+++ partials/cron.php
@@ -1,23 +1,29 @@
 <?php
 
-//* * * * * say "cron"
-//* * * * * /usr/bin/curl http://localhost:8888/odie/cron.php
+$url = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}/{$_SERVER['REQUEST_URI']}";
+$query = (parse_url($url, PHP_URL_QUERY));
+parse_str($query);
 
-  $dbhost = 'localhost';
-  $dbuser = 'root';
-  $dbpass = 'root';
-  $conn = mysqli_connect($dbhost, $dbuser, $dbpass, 'odie');
-  if(! $conn ) { die('Could not connect: ' . mysqli_error()); }
+if ($string == 'ok' || $argv['1'] == 'ok') { // checks for ?string=ok or cron argv 'ok'
 
-	$sql_get = "SELECT username FROM users ORDER BY RAND() LIMIT 0,1 ";
+	include('vars.php');
+
+	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	if(! $conn ) { die('Could not connect: ' . mysqli_error()); }
+
+	$sql_get = "SELECT username FROM users ORDER BY RAND() LIMIT 0,1 "; // order rows randomly and get 1
 	$query_get = mysqli_query( $conn, $sql_get );
 	$row = mysqli_fetch_array($query_get, MYSQL_ASSOC);
 	$username = $row['username'];
 
-	$sql_set = "UPDATE daily SET username = '$username'";
+	$sql_set = "UPDATE daily SET username = '$username'"; // set username on daily table to $username from random row
 	$query_set = mysqli_query( $conn, $sql_set );
 
 	mysqli_close($conn);
+
+} else {
+	die('no, fuck u'); // if querystring or cron argv doesnt check out
+}
 
 ?>
 
