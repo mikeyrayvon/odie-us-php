5807c7143fba2ba073dc545da99f0152de993c22
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-10-10 19:43:56 -0500

    improve string replacement. add url check

--- partials/insert.php
+++ partials/insert.php
@@ -6,35 +6,39 @@ include('vars.php');
 $link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
 // Check connection
-if($link === false){
-    die("ERROR: Could not connect.");
+if ($link === false){
+  die("ERROR: Could not connect.");
 }
- 
+
 // Strip tags for security
-$username_strip = strip_tags($_POST['username']);
-$url_strip = strip_tags($_POST['url']);
-$title_strip = strip_tags($_POST['title']);
-$description_strip = strip_tags($_POST['description']);
+$username = preg_replace('/[^a-zA-Z0-9\-]/', '', strip_tags($_POST['username']));
+$url = strip_tags($_POST['url']);
+$title = strip_tags($_POST['title']);
+$description = strip_tags($_POST['description']);
 
-// Escape for security
-$username = mysqli_real_escape_string($link, $username_strip);
-$url = mysqli_real_escape_string($link, $url_strip);
-$title = mysqli_real_escape_string($link, $title_strip);
-$description = mysqli_real_escape_string($link, $description_strip);
- 
-// attempt insert query execution
-$result = mysqli_query($link, "SELECT * FROM users WHERE username = '$username'");
+$gdocs_start = 'https://docs.google.com/document/d/';
+$gdocs_end = '/pub';
 
-if(mysqli_num_rows($result) == 0) {
-	$sql = "INSERT INTO `users` (username, url, title, description)
-	SELECT '$username', '$url', '$title', '$description'";
-	if(mysqli_query($link, $sql)){
-		$response = 'success'; 
-	} else{
-		$response = 'error'; 
-	}
-} else { //username already exists in db
-	$response = 'exists'; 
+
+if (substr($url, 0, strlen($gdocs_start)) !== $gdocs_start !! substr($url, -4) !== $gdocs_end) {
+  $response = 'badurl';
+} else {
+
+	// attempt insert query execution
+	$result = mysqli_query($link, "SELECT * FROM users WHERE username = '$username'");
+
+  if(mysqli_num_rows($result) == 0) {
+    $sql = "INSERT INTO `users` (username, url, title, description)
+    SELECT '$username', '$url', '$title', '$description'";
+    if(mysqli_query($link, $sql)){
+      $response = 'success'; 
+    } else{
+      $response = 'error'; 
+    }
+  } else { //username already exists in db
+    $response = 'exists';
+  }
+
 }
 
 // return response
